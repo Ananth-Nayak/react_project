8a71b9befcd14cc0d90b52da1821983ea44632fc
Ananth Nayak <nananthnayak@gmail.com>
2023-11-01 09:05:39 +0530

    optimization of app using parcel

--- App.js
+++ App.js
@@ -1,6 +1,26 @@
+/***
+ * Parcel Features:-
+ * Hot Module Replacement:-whenever we click save button it automatically reload webpage
+ * parcel will keep track of the all the files which are updating
+ * it uses file watcher algorithm written in c++                       [npx parcel build index.html(delete main:-app.js in package.json file or it will throw error)]
+ * it creates local server for developers to check the code
+ * it bundles all files to production ready
+ * minifier
+ * cleaning our code(like removing console.log)
+ * superfast
+ * image optimaisation
+ * caching while development
+ * compatible with older version of browser
+ * 
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading=React.createElement("h1",{
     id:"title1"
 },"Heading1");
+
 const heading2=React.createElement("h2",{
     id:"title2"
 },"Heading2");
@@ -8,6 +28,7 @@ const heading2=React.createElement("h2",{
 const container=React.createElement("div",{
     id:"container"
 },[heading,heading2]);
+
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 //passing a react element inside the root
