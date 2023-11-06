fc8c8dd311f1055f45066fef9024d195ea0215b6
Ananth Nayak <nananthnayak@gmail.com>
2023-11-07 00:33:52 +0530

    before hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,24 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurantList } from "../constants";
 
-const Body=()=>(
+const Body=()=>{
+  var searchTxt="Vaishali";
+
+  return (
+    <>
+    <div className="search-container">
+      <input 
+      className="search-input"
+      placeholder="Search"
+      value={searchTxt}
+      onChange={(e)=>{          //whenever we change in the input react will re render everything including the above variable value
+      searchTxt=e.target.value;  //so it render above searchtxt value adds with e.target.value(since searchtxt is hardcoded value)
+      console.log(searchTxt)    //therefor local variable(searchTxt) is not prefered in react due to re rendering 
+      }}                                              
+      />                                          
+      <button className="search-btn">Search</button>
+
+    </div>
     <div className="restaurant-list">
     {
       restaurantList.map((restaurant)=>(
@@ -9,7 +26,11 @@ const Body=()=>(
       ))
     }
     </div>
+  </>
+  )
+
+};
         
-    )
+    
 
 export default Body;    
